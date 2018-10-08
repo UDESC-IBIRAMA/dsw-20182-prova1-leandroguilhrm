ac37d5c773dd01b9bf50b90cda52c174a1d1496f
LEANDRO GUILHERME DA SILVA <07610115959@edu.udesc.br>
2018-10-08 20:55:44 -0300

    commit inicial

---
+++ src/main/java/br/udesc/prova1leandro/Prova1LeandroApplication.java
@@ -0,0 +1,12 @@
+package br.udesc.prova1leandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova1LeandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova1LeandroApplication.class, args);
+	}
+}
---
+++ src/test/java/br/udesc/prova1leandro/Prova1LeandroApplicationTests.java
@@ -0,0 +1,16 @@
+package br.udesc.prova1leandro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Prova1LeandroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
